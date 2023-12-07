3c3e2779eeec090faa8d96d80be92db120108047
Victor Dumitrescu <v.dumitrescu@outlook.com>
2023-12-07 11:01:18 +0100

    Switch tz4 to tz1

--- crates/jstz_cli/src/bridge/deposit.rs
+++ crates/jstz_cli/src/bridge/deposit.rs
@@ -4,7 +4,7 @@ use bs58;
 use crate::{config::Config, octez::OctezClient};
 
 pub fn exec(from: String, to: String, amount: u64, cfg: &Config) -> Result<()> {
-    // 1. Convert tz4 address to hexencoded bytes
+    // 1. Convert tz1 address to hexencoded bytes
     let to_bytes = bs58::decode(&to).into_vec().unwrap();
     let to_bytes = &to_bytes[3..to_bytes.len() - 4]; // Skip the first 3 bytes and the last 4 bytes
     let to_hex = hex::encode_upper(to_bytes);
